7c1242a7f24be1adcb3f6c7a56e0331dc6ff1598
sevi <sivisevi1@gmail.com>
2022-06-12 19:31:38 +0100

    Step 1.4, place the objects into an array, pass the props directly as an array

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -23,9 +23,12 @@ const Content = (props) =>
   return (
     
     <div>
-      <p>
-       Part: {props.part} {props.exercise}
-      </p>
+     
+       <p>Part: {props.parts[0].name} {props.parts[0].exercises} </p>
+       <p>Part: {props.parts[1].name} {props.parts[1].exercises}</p>
+       <p>Part: {props.parts[2].name} {props.parts[2].exercises}</p>
+      
+     
     </div>
   )
 
@@ -37,34 +40,34 @@ const Total = (props) =>
   console.log(props)
 return (
 
-    <p>Number of exercises {props.exercises}</p>
+    <p>Number of exercises  {props.parts[0].exercises +  props.parts[1].exercises + props.parts[2].exercises} </p>
   )
 
 }
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   
   
   return (
+  
     <div>
       <Header courseTitle = {course} />
-      <Content part = {part1.name} Content exercise = {part1.exercises}/>
-      <Content part = {part2.name} Content exercise = {part2.exercises}/>
-      <Content part = {part3.name} Content exercise = {part3.exercises}/>
-      <Total exercises = {part1.exercises + part2.exercises  + part3.exercises }/>
+      <Content parts = {parts} />  
+      <Total parts = {parts} />  
     </div>
 
 
