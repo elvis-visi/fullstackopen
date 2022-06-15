d86fc0b37eac37f0e7235c0f1c798ca3481790b0
sevi <sivisevi1@gmail.com>
2022-06-15 18:35:50 +0100

    1.14 displays the anecdote with the largest number of votes

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,11 +1,30 @@
 import { useState } from 'react'
 
-const Button = ({onClick,text}) => {
+const Button = ({ onClick, text }) => {
 
   return (
     <button onClick={onClick}>
       {text}
     </button>
+  )
+
+}
+
+const MaxDisplay = ({ votes, anec }) => {
+
+  //  find the max value in the array, and its index
+  let max = votes.reduce(function (a, b) {
+    return Math.max(a, b);
+  });
+  let maxIndex = votes.indexOf(max)
+
+  console.log(`max is ${max}`)
+  console.log(`max anac is ${maxIndex}`)
+  return (
+
+    <div>{anec[maxIndex]} <p>
+    </p>has {votes[maxIndex]} votes
+    </div>
   )
 
 }
@@ -21,42 +40,49 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
 
- const [selected, setSelected] = useState(0)
- const [vote, setVote] = useState(new Array(anecdotes.length).fill(0))
+  const [selected, setSelected] = useState(0)
+  const [vote, setVote] = useState(new Array(anecdotes.length).fill(0))
 
- console.log(`current votes: ${vote}`)
+  console.log(`current votes: ${vote}`)
   //update the selected anecdote using the setSelected function
   //pass a random selection, take the length of the array in consideration
 
-  let rand = Math.floor(Math.random() * anecdotes.length) 
-  console.log(`Current anecdote: ${selected}\n Random value: ${rand}`)
+  let rand
   const handleNextAnecdote = () => {
+    rand = Math.floor(Math.random() * anecdotes.length)
 
     setSelected(rand)
   }
-  
 
   //handle votes, increase the votes on click of button
   const handleVote = () => {
     //make copy of the array
     let arr = [...vote]
- 
+
 
     //vote goes to the current anecdote -> current random value
     arr[selected]++
     setVote(arr)
-   
+
   }
 
   let currentAnecdote = rand
 
+
+
+
   return (
     <div>
-      {anecdotes[selected]}
-      <div>
-      <Button onClick={handleVote}  text = "Vote"/> 
-      <Button onClick={handleNextAnecdote}  text = "Next anecdote"/>
-        </div>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {vote[selected]} votes</div>
+
+      <Button onClick={handleVote} text="Vote" />
+      <Button onClick={handleNextAnecdote} text="Next anecdote" />
+
+      <h1>Anecdote with most votes</h1>
+      <MaxDisplay votes={vote} anec={anecdotes} />
+
 
     </div>
   )
