1e2cec23460a5832b81e4363730755aa290e5707
sevi <sivisevi1@gmail.com>
2022-06-15 10:15:12 +0100

    Step 1.10, StatisticLine component always displays a single statistic

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,47 +11,47 @@ const Button = ({ onClick, text }) => {
   )
 }
 
-const Statistics = (props) => {
+const StatisticsLine = (props) => {
 
- 
-  //average: (good - bad) / total
-  let average = ((props.scores[0] - props.scores[2])/props.total) 
-
-  
- 
-//good +1 score, neutral= 0, bad = -1
-//positive, good over total
-
-console.log(average)
-
-if(!isNaN((average)))
-{
+  //text and value, e,.g Good 1
   return (
-    <div>
-      <div>Good : {props.scores[0]}</div>
-      <div>Neutral : {props.scores[1]}</div>
-      <div>Bad : {props.scores[2]}</div>
-      <div>All: {props.scores[0] + props.scores[1] + props.scores[2]}</div>
-      <div>Average: {average}</div>
-      <div>Positive: {((props.scores[0]/total) * 100)} % </div>
-   </div>
-   
+    <div> {props.text}  {props.value}</div>
   )
 }
+//Statistics scores={[good, neutral, bad]} total={total}
+const Statistics = (props) => {
 
 
+  //average: (good - bad) / total
+  let average = ((props.scores[0] - props.scores[2]) / props.total)
+  let positive = ` ${((props.scores[0] / total) * 100)} %`
 
-  return(
+
+  //good +1 score, neutral= 0, bad = -1
+  //positive, good over total
+
+  console.log(average)
+
+  if (!isNaN((average))) {
+    return (
+      <div>
+        <StatisticsLine text="Good" value={props.scores[0]} />
+        <StatisticsLine text="Neutral" value={props.scores[1]} />
+        <StatisticsLine text="Bad" value={props.scores[2]} />
+        <StatisticsLine text="All" value={props.scores[0] + props.scores[1] + props.scores[2]} />
+        <StatisticsLine text="Average" value={average} />
+        <StatisticsLine text="Positive" value={positive} />
+      </div>
+
+    )
+  }
+
+  return (
     <div>
       No feedback given
     </div>
 
   )
-
-  
-
-
- 
 
 }
 
@@ -62,26 +62,26 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
- 
+
 
   const handleGoodClick = () => {
     //update/increase the good state by 1
     setGood(good + 1)
     total++
-    console.log({total})
-}
+    console.log({ total })
+  }
   const handleNeutralClick = () => {
     //update/increase the neutral state by 1
     setNeutral(neutral + 1)
     total++
-    console.log({total})
-}
+    console.log({ total })
+  }
   const handleBadClick = () => {
     //update/increase the bad state by 1
     setBad(bad + 1)
     total++
-    console.log({total})
-}
+    console.log({ total })
+  }
 
   return (
     <div>
@@ -97,8 +97,8 @@ const App = () => {
         text="Bad" />
       <div>
         <h2>Statistics </h2>
-         <Statistics  scores  = {[good, neutral , bad]} 
-         total = {total}/> 
+        <Statistics scores={[good, neutral, bad]}
+          total={total} />
       </div>
     </div>
   )
