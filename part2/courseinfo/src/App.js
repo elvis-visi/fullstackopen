f64f64dfa93ce0045ba47bbeabc964cb0d1622e4
sevi <sivisevi1@gmail.com>
2022-06-16 20:40:15 +0100

    Part2

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+
+
+import Course from './components/Course'
+
+
+const App = () => {
+  
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Key-attribute',
+        exercises: 12,
+        id: 4
+      }
+    ]
+  }
+
+ 
+  return (
+    <Course course={course}/>
+  )
+
+  
+    
+  
+}
+
+export default App;
