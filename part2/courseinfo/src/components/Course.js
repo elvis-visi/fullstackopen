564265a1d7349665e57d0028ab8254c2cda46010
sevi <sivisevi1@gmail.com>
2022-06-17 09:46:54 +0100

    Add separate components folder

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,77 +1,67 @@
-  //course -> App passing the course object
-  const Course = ({course}) => {
-      //return the Header and Content
-      console.log(`Course`, {course});
-    return(
-        <div>
-         <Header  name = {course.name} />
-         <Content parts= {course.parts} />
-         <Total  parts= {course.parts} />
-        </div>
-    )
-  }
 
- //Total of the exercises
- const Total= ({parts}) => {
- //add up the parts.exercises of each array element
-   
- const total = parts.reduce((s, p) => {
+const Course = ({ course }) => {
+  console.log(`Course`, { course });
+ return (
+    <div key={course.id}>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+//Total of the exercises
+const Total = ({ parts }) => {
+  //add up the parts.exercises of each array element
+
+  const total = parts.reduce((s, p) => {
     console.log('what is happening', s, p)
     return s + p.exercises
-  },0)
+  }, 0)
 
   return (
 
     <div> Total of {total} exercises</div>
   )
 
+}
+
+// Course component passing the name property of course
+const Header = ({ name }) => {
+
+  return (
+
+    <h1>
+      {name}
+    </h1>
+  )
+}
+//Pass the parts property of course, which is an array
+const Part = ({ name, exercise }) => {
+  //return 1 part:  name exercise
+
+  console.log(`Part`,  { name }, { exercise })
+
+  return (
+    <p> {name} {exercise} </p>
+  )
 
 }
-     
- 
 
-  // Course component passing the name property of course
-  const Header = ({name}) => {
+const Content = ({ parts }) => {
+  // properties : name exercise id 
+  //name + exercise
+  console.log(`Content `, { parts });
 
-      return (
+  return (
+    parts.map(parts =>
+      <Part key={parts.id} name={parts.name}
+        exercise={parts.exercises} />
 
-        <h1>
-            {name}
-        </h1>
-      )
-  }
- //Pass the parts property of course, which is an array
- const Part = ({id,name,exercise}) => {
- //return 1 part:  name exercise
+    )
 
- console.log(`Part`, {id},{name} ,{exercise})
-
-   return(
-       <p key={id}> {name} {exercise} </p>
-   )
-
-
- }
-
-  const Content = ({parts}) => {
- // properties : name exercise id 
-    //name + exercise
-    console.log(`Content `, {parts});
-
-    return (
-        parts.map(parts => 
-        <Part id = {parts.id} name = {parts.name} 
-        exercise = {parts.exercises} />
-            
-            )
-
-            )
-
-
-        //return the Part
-    
-
-  }
+  )
+}
 
 
 
