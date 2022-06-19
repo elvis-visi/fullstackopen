8941b3f912e05cc87477a544d476aadd7382ea93
sevi <sivisevi1@gmail.com>
2022-06-19 13:26:30 +0100

    Filter case insensitive

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const Display = ({ name, number }) => {
 
@@ -11,8 +11,10 @@ const App = () => {
 
   //persons -> where we will store the names of the phonebook
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-  number:'123-4566'}
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
   console.log("persons", persons);
 
@@ -24,23 +26,67 @@ const App = () => {
   const[newNumber, setNewNumber] = useState('')
   console.log('new number', newNumber)
 
+  //search field
+  const[filterByName, setNewFilter] = useState('')
+
+  const[idPerson, setNewId] = useState(persons.length + 1)
+  console.log("id Person" ,idPerson)
+
+  const handleNameFilter = (event) => {
+    console.log("event target value", event.target.value)
+    setNewFilter(event.target.value)
+  }
+
+  const peopleToShow = persons.filter(per => per.name.toLocaleLowerCase().includes(filterByName.toLowerCase()))
+  console.log("people to show",peopleToShow)
+
+  //if filterByName 
+    let phoneBookToShow 
+
+    if(filterByName.length != 0 && peopleToShow.length != 0)
+    {
+      phoneBookToShow = peopleToShow
+
+    }else{
+      phoneBookToShow = persons
+    }
+
+
   //event parameter is the event that triggers the call to the event handler
   //function
+
+  useEffect (() => {setNewId(persons.length + 1)})
+
   const addPerson = (event) => {
     console.group("event", event.target) //the form
     event.preventDefault() //prevent default action of submitting HTML forms
+  
 
     //add new note
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
+      id : idPerson
+      //id: 
     }
 
     //if personObjec is already in the array, alert()
     let isDuplicate = false
-    for(let i = 0; i< persons.length; i++)
+
+    //compare the name and number only, ingore the id
+    const personCompare = persons.map(function(per)
     {
-        if( JSON.stringify(personObject) == JSON.stringify(persons[i]))
+      return per.name + per.number
+    })
+  
+    console.log("comparison", personCompare)
+
+    for(let i = 0; i< personCompare.length; i++)
+    {
+          const obj = personObject.name + personObject.number
+    //
+
+        if( JSON.stringify(obj) === JSON.stringify(personCompare[i]))
         {
            alert(`${newName} - ${newNumber} is already added to phonebook`)
            isDuplicate = true
@@ -79,7 +125,11 @@ const App = () => {
 
     <div>
       <h2>Phonebook</h2>
+       filter shown with <input value={filterByName}
+       onChange={handleNameFilter}/>
+     
       <form onSubmit={addPerson} >
+       <h1>add a new</h1>
         <div>
           name: <input value={newName}
             onChange={handleNameChange} />
@@ -96,8 +146,8 @@ const App = () => {
       <h2>Numbers</h2>
 
 
-      {persons.map(per =>
-        <Display name={per.name} number={per.number} />
+      {phoneBookToShow.map(per =>
+        <Display key= {per.id} name={per.name} number={per.number} />
 
       )}
 
