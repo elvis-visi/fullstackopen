e4955021519eda67783f8f31de767499be954811
sevi <sivisevi1@gmail.com>
2022-07-18 22:09:55 +0100

    4.4: helper functions and unit tests

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -1,10 +1,20 @@
+//receive and empty array of blogs and rerturn 1
 const dummy = (blogs) => {
 
   return 1
 }
 
-module.exports = {
-  dummy
+
+//return the total sum of the likes in all the blog posts
+const totalLikes = (blogs) => {
+  if(blogs.length === 0)
+    return 0
+
+  return blogs.reduce((sum,blog) => sum += blog.likes,0)
+
 }
 
-//reverse.test.js
+
+module.exports = {
+  dummy, totalLikes
+}
