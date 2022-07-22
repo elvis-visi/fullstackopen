63fb20743e9f5675189661e822f1b569a9eebc3e
sevi <sivisevi1@gmail.com>
2022-07-22 18:47:48 +0100

    4.11-4-12, likes or title/url missing

--- part4/blog/models/blog.js
+++ part4/blog/models/blog.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
-  //tranform dedault _id property of Mongoose objects to id
-  blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+//tranform dedault _id property of Mongoose objects to id
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-  module.exports = mongoose.model('Blog', blogSchema)
+module.exports = mongoose.model('Blog', blogSchema)
--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -1,4 +1,4 @@
-//import express application, pass it to supertest function. 
+//import express application, pass it to supertest function.
 //import http so we can test HTTP requests
 
 const mongoose = require('mongoose') //need connection to mongoDb
@@ -51,16 +51,77 @@ test('bogs posts are returned as JSON', async () => {
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-},100000) 
+},100000)
 
 test('unique identifier property of the blog posts is named id', async () => {
-    const response  = await api.get('/api/blogs')
+  const response  = await api.get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 
-    expect(response.body[0].id.toBedefined)
-    expect(response.body[0]._id).toBe(undefined)
+  expect(response.body[0].id.toBedefined)
+  expect(response.body[0]._id).toBe(undefined)
 
+})
+
+//verify new post has been added => number of total posts has increased by 1
+test('a valid blog post has been added', async () => {
+  const newPost =  {  //new post to be added
+    'title': 'react',
+    'author': 'lejm',
+    'url': 'react.web',
+    'likes': 11
+
+  }
+
+  await api
+    .post('/api/blogs')  //post request to the url
+    .send(newPost)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response  =await api.get(('/api/blogs')) //successful operation or not
+
+  const contents = response.body.map(r => r.title) //new array with title of each blog post
+
+  //expect total number of posts to have increased by 1
+  expect(response.body).toHaveLength(initialBlogs.length + 1)
+  //assuring the correct post has been added to the database => same title
+  expect(contents).toContain('react')
+
+
+})
+
+//if likes property is missing from the from the request, it will default to 0
+test('likes property missing , likes will default to 0', async () => {
+  const newPost =  {  //new post to be added
+    'title': 'react',
+    'author': 'lejm',
+    'url': 'react.web',
+  }
+
+  const response = await api
+    .post('/api/blogs')
+    .send(newPost)
+    .expect('Content-Type',/json/)
+    .expect(201)
+
+
+  expect(response.body.likes).toBeDefined()
+  expect(response.body.likes).toBe(0)
+})
+
+test('verify if title and url are in the body', async () => {
+
+  const newPost = {
+    author: 'deku',
+    likes: 16
+  }
+  const response = await api
+    .post('/api/blogs')
+    .send(newPost)
+    .expect(400)
+
+  expect(response.body.error).toBe('title or url is missing')
 })
 
 
